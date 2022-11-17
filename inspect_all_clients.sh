17524b235553f77512e55f731420a2b785a9ad50
Peter Möller <peter.moller@cs.lth.se>
2022-11-17 08:57:47 +0100

    Did some sound quoting of variables

--- inspect_all_clients.sh
+++ inspect_all_clients.sh
@@ -6,7 +6,7 @@ cd /tmp
 
 # 'SELECTION' is the domain or domains to be reported. 
 # It is assumed to be a single domain such as CS_CLIENTS, but may be more.
-SELECTION="$(echo $@ | tr '[:upper:]' '[:lower:]')"  # Ex: SELECTION='cs_clients'
+SELECTION="$(echo "$@" | tr '[:upper:]' '[:lower:]')"  # Ex: SELECTION='cs_clients'
 
 # We must, however, have at least one domain to go through
 if [ -z "$SELECTION" ]; then
@@ -39,10 +39,10 @@ fi
 # Also, generate a explanatory string for the domains ('Explanation'):
 for DOMAIN in $SELECTION; do
     # Test if the domain exists
-    if dsmadmc -id=$ID -password=$PASSWORD -DISPLaymode=LISt "query domain $DOMAIN" &>/dev/null; then
-        Explanation+="$DOMAIN ($(dsmadmc -id=$ID -password=$PASSWORD -DISPLaymode=list  "query domain $DOMAIN" | grep -E "^\s*Description:" | cut -d: -f2 | sed 's/^\ *//')) & "
+    if dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=LISt "query domain $DOMAIN" &>/dev/null; then
+        Explanation+="$DOMAIN ($(dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=list  "query domain $DOMAIN" | grep -E "^\s*Description:" | cut -d: -f2 | sed 's/^\ *//')) & "
         # Ex: Explanation+='CS_CLIENTS (CS client domain) & '
-        CLIENTStmp+="$(dsmadmc -id=$ID -password=$PASSWORD -DISPLaymode=list "query node * domain=$DOMAIN" | grep -E "^\s*Node Name:" | awk '{print $NF}')"
+        CLIENTStmp+="$(dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=list "query node * domain=$DOMAIN" | grep -E "^\s*Node Name:" | awk '{print $NF}')"
         # Ex: CLIENTStmp+='CS-ABRUCE
         #                  CS-DRIFTPC
         #                  CS-PETERMAC
@@ -105,7 +105,7 @@ FormatStringConten="${Client_W}${BackedupNumfiles_W}${TransferredVolume_W}  ${Ba
 
 
 server_info() {
-    ServerInfo="$(dsmadmc -id=$ID -password=$PASSWORD -DISPLaymode=LISt "query status")"
+    ServerInfo="$(dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=LISt "query status")"
     ServerVersion="$(echo "$ServerInfo" | grep -E "^\s*Server Version\s" | grep -Eo "[0-9]*" | tr '\n' '.' | cut -d\. -f1-3)"  # Ex: ServerVersion=8.1.16
     #ServerName="$(echo "$ActlogToday" | grep "Session established with server" | cut -d: -f1 | awk '{print $NF}')"
     ServerName="$(echo "$ServerInfo" | grep "Server Name:" | cut -d: -f2 | sed 's/^ //')"                             # Ex: ServerName='TSM3'
@@ -114,7 +114,7 @@ server_info() {
 }
 
 client_info() {
-    ClientInfo="$(dsmadmc -id=$ID -password=$PASSWORD -DISPLaymode=LISt "query node $client f=d")"
+    ClientInfo="$(dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=LISt "query node $client f=d")"
     ClientVersion="$(echo "$ClientInfo" | grep -E "^\s*Client Version:" | cut -d: -f2 | sed -e 's/ Version //' -e 's/, release /./' -e 's/, level /./' | cut -d. -f1-3)"   # Ex: ClientVersion='8.1.13'
     ClientLastNetworkTemp="$(echo "$ClientInfo" | grep -Ei "^\s*TCP/IP Address:" | cut -d: -f2 | sed -e 's/^ //')"                                                         # Ex: ClientLastNetworkTemp='10.7.58.184'
     case "$(echo "$ClientLastNetworkTemp" | cut -d\. -f1-2)" in
@@ -139,15 +139,15 @@ client_info() {
     fi
     # Ex: ClientOS='Macintosh' / 'Ubuntu 20.04.4 LTS' / 'Windows 10 Education' / 'Fedora release 36' / 'Debian GNU/Linux 10' / 'CentOS Linux 7.9.2009'
     ClientLastAccess="$(echo "$ClientInfo" | grep -Ei "^\s*Last Access Date/Time:" | cut -d: -f2-)"     # Ex: ClientLastAccess='2018-11-01   11:39:06'
-    ClientTotalSpaceTemp="$(LANG=en_US dsmadmc -id=$ID -password=$PASSWORD -DISPLaymode=LISt "query occup $client" | grep "Physical Space Occupied" | cut -d: -f2 | sed 's/ -/0/' | sed 's/,//g' | tr '\n' '+' | sed 's/+$//')"  # Ex: ClientTotalSpaceTemp=' 217155.02+ 5.20+ 1285542.38'
+    ClientTotalSpaceTemp="$(LANG=en_US dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=LISt "query occup $client" | grep "Physical Space Occupied" | cut -d: -f2 | sed 's/ -/0/' | sed 's/,//g' | tr '\n' '+' | sed 's/+$//')"  # Ex: ClientTotalSpaceTemp=' 217155.02+ 5.20+ 1285542.38'
     ClientTotalSpaceUsedMB=$(echo "scale=0; $ClientTotalSpaceTemp" | bc | cut -d. -f1)                                                                                                                      # Ex: ClientTotalSpaceUsedMB=1502702
-    ClientTotalNumfilesTemp="$(LANG=en_US dsmadmc -id=$ID -password=$PASSWORD -DISPLaymode=LISt "query occup $client" | grep "Number of Files" | cut -d: -f2 | sed 's/ -/0/' | sed 's/,//g' | tr '\n' '+' | sed 's/+$//')"       # ClientTotalNumfilesTemp=' 1194850+ 8+ 2442899'
+    ClientTotalNumfilesTemp="$(LANG=en_US dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=LISt "query occup $client" | grep "Number of Files" | cut -d: -f2 | sed 's/ -/0/' | sed 's/,//g' | tr '\n' '+' | sed 's/+$//')"       # ClientTotalNumfilesTemp=' 1194850+ 8+ 2442899'
     ClientTotalNumFiles=$(echo "scale=0; $ClientTotalNumfilesTemp" | bc | cut -d. -f1)                                                                                                                      # Ex: ClientTotalNumFiles=1502702
     # The following is no longer used since it's A) wrong and B) awk summaries in scientific notation which is not desirable. Kept here for some reason...
-    #ClientTotalSpaceUsedMB="$(dsmadmc -id=$ID -password=$PASSWORD -DISPLaymode=LISt "query occup $client" | awk '/Physical Space Occupied/ {print $NF}' | sed 's/,//' | awk '{ sum+=$1 } END {print sum}' | cut -d. -f1)"
-    #ClientTotalNumFiles="$(dsmadmc -id=$ID -password=$PASSWORD -DISPLaymode=LISt "query occup $client" | awk '/Number of Files/ {print $NF}' | sed 's/,//' | awk '{ sum+=$1 } END {print sum}')"
+    #ClientTotalSpaceUsedMB="$(dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=LISt "query occup $client" | awk '/Physical Space Occupied/ {print $NF}' | sed 's/,//' | awk '{ sum+=$1 } END {print sum}' | cut -d. -f1)"
+    #ClientTotalNumFiles="$(dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=LISt "query occup $client" | awk '/Number of Files/ {print $NF}' | sed 's/,//' | awk '{ sum+=$1 } END {print sum}')"
     # Get the number of file spaces on the client
-    ClientNumFilespaces=$(dsmadmc -id=$ID -password=$PASSWORD -DISPLaymode=LISt "query filespace $client f=d" | grep -cE "^\s*Filespace Name:")   # Ex: ClientNumFilespaces=8
+    ClientNumFilespaces=$(dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=LISt "query filespace $client f=d" | grep -cE "^\s*Filespace Name:")   # Ex: ClientNumFilespaces=8
 }
 
 backup_result() {
@@ -205,11 +205,11 @@ backup_result() {
             # Update 2022-10-23: I now know how to get the last date of backup: 
             # Do a 'query filespace $client f=d' and look for "Days Since Last Backup Completed:"
             # Note that it will be one day per filespace (file system)
-            NumDaysSinceLastBackup="$(dsmadmc -id=$ID -password=$PASSWORD -DISPLaymode=LISt  "query filespace $client f=d" | grep -E "^\s*Days Since Last Backup Completed:" | cut -d: -f2 | sed 's/[, <]//g' | sort -u)"
+            NumDaysSinceLastBackup="$(dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=LISt  "query filespace $client f=d" | grep -E "^\s*Days Since Last Backup Completed:" | cut -d: -f2 | sed 's/[, <]//g' | sort -u)"
             # Ex: NumDaysSinceLastBackup='1
             #     298
             #     339'
-            LastBackupDate="$(dsmadmc -id=$ID -password=$PASSWORD -DISPLaymode=LISt  "query filespace $client f=d" | grep -E "^\s*Last Backup Completion Date/Time:" | cut -d: -f2 | awk '{print $1}' | sort -V | sort --field-separator='/' -k 3,3 -k 2,2 -k 1,1 | uniq)"
+            LastBackupDate="$(dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=LISt  "query filespace $client f=d" | grep -E "^\s*Last Backup Completion Date/Time:" | cut -d: -f2 | awk '{print $1}' | sort -V | sort --field-separator='/' -k 3,3 -k 2,2 -k 1,1 | uniq)"
             # Ex: LastBackupDate='09/23/17
             #     01/05/20
             #     04/12/21
@@ -229,7 +229,7 @@ backup_result() {
 error_detection() {
     #ErrorMsg=""
     # First: see if there's no schedule associated with the node
-    if [ -z "$(dsmadmc -id=$ID -password=$PASSWORD -DISPLaymode=LISt "query schedule * node=$client" 2>/dev/null | grep -Ei "^\s*Schedule Name:")" ]; then
+    if [ -z "$(dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=LISt "query schedule * node=$client" 2>/dev/null | grep -Ei "^\s*Schedule Name:")" ]; then
         ErrorMsg+="--- NO SCHEDULE ASSOCIATED ---"
     fi
     if [ -n "$(echo "$DualExecutionsToday" | grep -E "\b$client\b")" ]; then
@@ -278,11 +278,11 @@ server_info
 
 # Get the activity log for today (saves time to do it only one)
 # Do not include 'ANR2017I Administrator ADMIN issued command:'
-ActlogToday="$(dsmadmc -id=$ID -password=$PASSWORD -TABdelimited "query act begindate=today begintime=00:00:00 enddate=today endtime=now" | grep -v "ANR2017I")"
+ActlogToday="$(dsmadmc -id="$ID" -password="$PASSWORD" -TABdelimited "query act begindate=today begintime=00:00:00 enddate=today endtime=now" | grep -v "ANR2017I")"
 # Get a notification if a client have more than one 'ANE4961I'; if so, there are two clients executing and that should be rectified
 DualExecutionsToday="$(echo "$ActlogToday" | grep ANE4961I | awk '{print $7}' | sed 's/)//' | sort | uniq -d)"  # Ex: DualExecutionsToday=NIKLAS
 # Get all concluded executions (ANR2579E or ANR2507I) the last year. This will save a lot of time later on
-AllConcludedBackups="$(dsmadmc -id=$ID -password=$PASSWORD -TABdelimited "query act begindate=today-$ActLogLength enddate=today" | grep -E "ANR2579E|ANR2507I")"
+AllConcludedBackups="$(dsmadmc -id="$ID" -password="$PASSWORD" -TABdelimited "query act begindate=today-$ActLogLength enddate=today" | grep -E "ANR2579E|ANR2507I")"
 
 echo "$Today $(date +%T): Backup report for ${Explanation% & } on server $ServerName (Spectrum Protect version $ServerVersion)" > $ReportFile
 printf "$FormatStringHeader\n" "CLIENT" "NumFiles" "Transferred" "Duration" "Status" " ∑ files" "Total [MB]" "# FS" "Version" "Client network" "Client OS" "Errors" >> $ReportFile
