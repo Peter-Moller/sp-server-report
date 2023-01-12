9a90b2c998836186861f4988428e7dd9b36e76db
Peter Moller <peter.moller@cs.lth.se>
2023-01-12 08:53:57 +0100

    Major changes:skipped text reporting and instead use html-reporting. Supports scp to server

--- inspect_all_clients.sh
+++ inspect_all_clients.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-cd /tmp
 # Get detailed info for clients using the activity log
 # 2022-11-11 / PM
 # Department of Computer Science, Lund University
 
 # 'SELECTION' is the domain or domains to be reported. 
 # It is assumed to be a single domain such as CS_CLIENTS, but may be more.
-SELECTION="$(echo "$@" | tr '[:upper:]' '[:lower:]')"  # Ex: SELECTION='cs_clients'
+export SELECTION="$(echo "$@" | tr '[:upper:]' '[:lower:]')"  # Ex: SELECTION='cs_clients'
 
 # We must, however, have at least one domain to go through
 if [ -z "$SELECTION" ]; then
@@ -20,13 +19,13 @@ if [ -L "${BASH_SOURCE[0]}" ]; then
     # Get the *real* directory of the script
     ScriptDirName="$(dirname "$(readlink "${BASH_SOURCE[0]}")")"   # ScriptDirName='/usr/local/bin'
     # Get the *real* name of the script
-    ScriptName="$(basename "$(readlink "${BASH_SOURCE[0]}")")"     # ScriptName='moodle_backup.sh'
+    ScriptName="$(basename "$(readlink "${BASH_SOURCE[0]}")")"     # ScriptName=inspect_all_clients
 else
-    ScriptDirName="$(dirname "${BASH_SOURCE[0]}")"
+    ScriptDirName="$(dirname "${BASH_SOURCE[0]}")"                 # ScriptDirName=/home/cs-pmo/tsm-server-report
     # What is the name of the script?
-    ScriptName="$(basename "${BASH_SOURCE[0]}")"
+    ScriptName="$(basename "${BASH_SOURCE[0]}")"                   # ScriptName=inspect_all_clients.sh
 fi
-ScriptFullName="${ScriptDirName}/${ScriptName}"
+ScriptFullName="${ScriptDirName}/${ScriptName}"                    # ScriptFullName=/home/cs-pmo/tsm-server-report/inspect_all_clients.sh
 
 # Get the secret password, either from the users home-directory or the script-dir
 if [ -f ~/.tsm_secrets.env ]; then
@@ -40,7 +39,7 @@ fi
 for DOMAIN in $SELECTION; do
     # Test if the domain exists
     if dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=LISt "query domain $DOMAIN" &>/dev/null; then
-        Explanation+="$DOMAIN ($(dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=list  "query domain $DOMAIN" | grep -E "^\s*Description:" | cut -d: -f2 | sed 's/^\ *//')) & "
+        Explanation+="“$DOMAIN” ($(dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=list  "query domain $DOMAIN" | grep -E "^\s*Description:" | cut -d: -f2 | sed 's/^\ *//')) & "
         # Ex: Explanation+='CS_CLIENTS (CS client domain) & '
         CLIENTStmp+="$(dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=list "query node * domain=$DOMAIN" | grep -E "^\s*Node Name:" | awk '{print $NF}')"
         # Ex: CLIENTStmp+='CS-ABRUCE
@@ -66,34 +65,17 @@ fi
 Today="$(date +%F)"                                      # Ex: Today=2011-11-11
 Now=$(date +%s)                                          # Ex: Now=1662627432
 OutDirPrefix="/tmp/tsm"
-#OutDir="$OutDirPrefix/${SELECTION// /+}"                # Ex: OutDir='/tmp/tsm/cs_servers+cs_clients'
-OutDir="$OutDirPrefix/clients"                           # Ex: OutDir='/tmp/tsm/clients"
+OutDir="$OutDirPrefix/${SELECTION/_/\/}"                 # Ex: OutDir=/tmp/tsm/cs/clients
 # Create the OutDir if it doesn't exist:
 if [ ! -d $OutDir ]; then
     mkdir -p $OutDir
 fi
-ReportFile="${OutDirPrefix}/${SELECTION// /+}_${Today}"  # Ex: ReportFile='/tmp/tsm/cs_servers+cs_clients_2022-11-11'
 
-Client_W="%-15s"
-BackedupNumfiles_H="%11s"
-BackedupNumfiles_W="%'11d"
-TransferredVolume_W="%13s"
-BackeupElapsedtime_W="%-10s"
-BackupStatus_W="%-23s"
-ClientTotalSpaceUseMB_WH="%10s"
-ClientTotalSpaceUsedMB_W="%'10d"
-ClientTotalNumFile_WH="%11s"
-ClientTotalNumFiles_W="%'11d"
-ClientNumFilespaces_WH="%6s"
-ClientNumFilespaces_W="%'6d"
-ClientVersion_W="%-9s"
-ClientLastNetWork_W="%-18s"
-ClientOS_W="%-23s"
-ErrorMsg_W="%-60s"
-
-# Format strings for all the printing:
-FormatStringHeader="${Client_W}${BackedupNumfiles_H}${TransferredVolume_W}  ${BackeupElapsedtime_W}${BackupStatus_W}  ${ClientTotalNumFile_WH}  ${ClientTotalSpaceUseMB_WH}  ${ClientNumFilespaces_WH}  ${ClientVersion_W}${ClientLastNetWork_W}${ClientOS_W}${ErrorMsg_W}"
-FormatStringConten="${Client_W}${BackedupNumfiles_W}${TransferredVolume_W}  ${BackeupElapsedtime_W}${BackupStatus_W}${ClientTotalNumFiles_W}  ${ClientTotalSpaceUsedMB_W}  ${ClientNumFilespaces_W}  ${ClientVersion_W}${ClientLastNetWork_W}${ClientOS_W}${ErrorMsg_W}"
+HTML_Template_Head="$ScriptDirName"/report_head.html
+HTML_Template_End="$ScriptDirName"/report_end.html
+### DECISION: should we have date in the file name for the overview table?
+### (it will be removed when copied to the web server)
+ReportFileHTML="${OutDirPrefix}/${SELECTION/_/\/}_${Today}.html"  # Ex: ReportFileHTML='/tmp/tsm/cs_servers+cs_clients_2022-11-11.html'
 
 
 #   _____   _____    ___   ______   _____       _____  ______      ______   _   _   _   _   _____   _____   _____   _____   _   _   _____ 
@@ -111,12 +93,13 @@ server_info() {
     ServerName="$(echo "$ServerInfo" | grep "Server Name:" | cut -d: -f2 | sed 's/^ //')"                             # Ex: ServerName='TSM3'
     ActLogLength="$(echo "$ServerInfo" | grep "Activity Log Retention:" | cut -d: -f2 | awk '{print $1}')"            # Ex: ActLogLength=30
     EventLogLength="$(echo "$ServerInfo" | grep "Event Record Retention Period:" | cut -d: -f2 | awk '{print $1}')"   # Ex: EventLogLength=14
+    OC_URL="https://${OC_SERVER}/oc/gui#clients/detail?server=${ServerName}&resource=BACKUPNODE&vmOwner=%20&target=%20&type=1&nodeType=1&ossm=0&nav=overview"
 }
 
 client_info() {
     ClientInfo="$(dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=LISt "query node $client f=d")"
-    ClientVersion="$(echo "$ClientInfo" | grep -E "^\s*Client Version:" | cut -d: -f2 | sed -e 's/ Version //' -e 's/, release /./' -e 's/, level /./' | cut -d. -f1-3)"   # Ex: ClientVersion='8.1.13'
-    ClientLastNetworkTemp="$(echo "$ClientInfo" | grep -Ei "^\s*TCP/IP Address:" | cut -d: -f2 | sed -e 's/^ //')"                                                         # Ex: ClientLastNetworkTemp='10.7.58.184'
+    ClientVersion="$(echo "$ClientInfo" | grep -E "^\s*Client Version:" | cut -d: -f2 | sed 's/ Version //' | sed 's/, release /./' | sed 's/, level /./' | cut -d. -f1-3)"   # Ex: ClientVersion='8.1.13'
+    ClientLastNetworkTemp="$(echo "$ClientInfo" | grep -Ei "^\s*TCP/IP Address:" | cut -d: -f2 | sed 's/^ //')"                                                         # Ex: ClientLastNetworkTemp='10.7.58.184'
     case "$(echo "$ClientLastNetworkTemp" | cut -d\. -f1-2)" in
         "130.235") ClientLastNetwork="LU" ;;
         "10.4")    ClientLastNetwork="Static VPN" ;;
@@ -131,7 +114,7 @@ client_info() {
         "130.235.17" ) ClientLastNetwork="CS server net" ;;
         "10.0.16"    ) ClientLastNetwork="CS client net" ;;
     esac
-    ClientOS="$(echo "$ClientInfo" | grep -Ei "^\s*Client OS Name:" | cut -d: -f3 | sed -e 's/Microsoft //' -e 's/ release//' | cut -d\( -f1)"
+    ClientOS="$(echo "$ClientInfo" | grep -Ei "^\s*Client OS Name:" | cut -d: -f3 | sed 's/Microsoft //' | sed 's/ release//' | cut -d\( -f1)"
     # Get some more info about macOS:
     if [ "$ClientOS" = "Macintosh" ]; then
         OSver="$(echo "$ClientInfo" | grep -Ei "^\s*Client OS Level:" | cut -d: -f2)"
@@ -149,7 +132,7 @@ client_info() {
     fi
     ClientTotalSpaceTemp="$(echo "$ClientOccupancy" | grep "$OccupiedPhrase" | cut -d: -f2 | sed 's/,//g' | tr '\n' '+' | sed 's/+$//')"              # Ex: ClientTotalSpaceTemp=' 217155.02+ 5.20+ 1285542.38'
     ClientTotalSpaceUsedMB=$(echo "scale=0; $ClientTotalSpaceTemp" | bc | cut -d. -f1)                                                                # Ex: ClientTotalSpaceUsedMB=1502702
-    ClientTotalNumfilesTemp="$(echo "$ClientOccupancy" | grep "Number of Files" | cut -d: -f2 | sed 's/,//g' | tr '\n' '+' | sed 's/+$//')"            # ClientTotalNumfilesTemp=' 1194850+ 8+ 2442899'
+    ClientTotalNumfilesTemp="$(echo "$ClientOccupancy" | grep "Number of Files" | cut -d: -f2 | sed 's/,//g' | tr '\n' '+' | sed 's/+$//')"           # ClientTotalNumfilesTemp=' 1194850+ 8+ 2442899'
     ClientTotalNumFiles=$(echo "scale=0; $ClientTotalNumfilesTemp" | bc | cut -d. -f1)                                                                # Ex: ClientTotalNumFiles=1502702
     # Get the number of file spaces on the client
     ClientNumFilespaces=$(dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=LISt "query filespace $client f=d" | grep -cE "^\s*Filespace Name:")   # Ex: ClientNumFilespaces=8
@@ -159,9 +142,9 @@ backup_result() {
     # Number of files:
     # (note that some client use a unicode 'non breaking space', e280af, as thousands separator. This must be dealt with!)
     # (also, note that some machines will have more than one line of reporting. We only consider the last one)
-    BackedupNumfiles="$(grep ANE4954I $ClientFile | sed 's/\xe2\x80\xaf/,/' | grep -Eo "Total number of objects backed up:\s*[0-9,]*" | awk '{print $NF}' | sed -e 's/,//g' | tail -1)"  # Ex: BackedupNumfiles='3483'
-    TransferredVolume="$(grep ANE4961I $ClientFile | grep -Eo "Total number of bytes transferred:\s*[0-9,.]*\s[KMG]?B" | tail -1 | cut -d: -f2 | sed -e 's/\ *//' | tail -1)"            # Ex: TransferredVolume='1,010.32 MB'
-    BackeupElapsedtime="$(grep ANE4964I $ClientFile | grep -Eo "Elapsed processing time:\s*[0-9:]*" | tail -1 | awk '{print $NF}' | tail -1)"                                            # Ex: BackedupElapsedtime='00:46:10'
+    BackedupNumfiles="$(grep ANE4954I $ClientFile | sed 's/\xe2\x80\xaf/,/' | grep -Eo "Total number of objects backed up:\s*[0-9,]*" | awk '{print $NF}' | sed 's/,//g' | tail -1)"  # Ex: BackedupNumfiles='3483'
+    TransferredVolume="$(grep ANE4961I $ClientFile | grep -Eo "Total number of bytes transferred:\s*[0-9,.]*\s[KMG]?B" | tail -1 | cut -d: -f2 | sed 's/\ *//' | tail -1)"            # Ex: TransferredVolume='1,010.32 MB'
+    BackeupElapsedtime="$(grep ANE4964I $ClientFile | grep -Eo "Elapsed processing time:\s*[0-9:]*" | tail -1 | awk '{print $NF}' | tail -1)"                                         # Ex: BackedupElapsedtime='00:46:10'
     # So, did it end successfully (ANR2507I)?
     if [ -n "$(grep ANR2507I $ClientFile)" ]; then
         BackupStatus="Successful"
@@ -183,7 +166,9 @@ backup_result() {
         LastUnsuccessfulNumDays=$(echo "$((Now - EpochtimeLastUnsuccessfulBackup)) / 81400" | bc)                                            # Ex: LastSuccessfulNumDays=1
         # If there is a successful backup in the total history, get when that was
         if [ -n "$LastSuccessfulBackup" ]; then
-            if [ $LastSuccessfulNumDays -eq 1 ]; then
+            if [ $LastSuccessfulNumDays -eq 0 ]; then
+                BackupStatus="Successful"
+            elif [ $LastSuccessfulNumDays -eq 1 ]; then
                 BackupStatus="Yesterday"
             else
                 BackupStatus="$LastSuccessfulNumDays days ago"
@@ -237,21 +222,18 @@ error_detection() {
     if [ -z "$(dsmadmc -id="$ID" -password="$PASSWORD" -DISPLaymode=LISt "query schedule * node=$client" 2>/dev/null | grep -Ei "^\s*Schedule Name:")" ]; then
         ErrorMsg+="--- NO SCHEDULE ASSOCIATED ---"
     fi
-    if [ -n "$(echo "$DualExecutionsToday" | grep -E "\b$client\b")" ]; then
-        ErrorMsg+="Dual executions (investigate!); "
-    fi
     if [ -n "$(grep ANE4007E "$ClientFile")" ]; then
-        ErrorMsg+="ANE4007E (access denied to object); "
+        ErrorMsg+="<a href=\"https://www.ibm.com/docs/en/spectrum-protect/8.1.17?topic=list-ane4000e#ANE4007E\" target=\"_blank\" rel=\"noopener noreferrer\">ANE4007E</a> (access denied to object); "
     fi
     if [ -n "$(grep ANR2579E "$ClientFile")" ]; then
-        ErrorCodes="$(grep ANR2579E "$ClientFile" | grep -Eio "\(return code -?[0-9]*\)" | sed -e 's/(//' -e 's/)//' | sort -u | tr '\n' ',' | sed -e 's/,c/, c/g' -e 's/,$//')"
-        ErrorMsg+="ANR2579E ($ErrorCodes); "
+        ErrorCodes="$(grep ANR2579E "$ClientFile" | grep -Eio "\(return code -?[0-9]*\)" | sed 's/(//' | sed 's/)//' | sort -u | tr '\n' ',' | sed 's/,c/, c/g' | sed 's/,$//')"
+        ErrorMsg+="<a href=\"https://www.ibm.com/docs/en/spectrum-protect/8.1.16?topic=list-anr0010w#ANR2579E\" target=\"_blank\" rel=\"noopener noreferrer\">ANR2579E</a> ($ErrorCodes); "
     fi
     if [ -n "$(grep ANR0424W "$ClientFile")" ]; then
-        ErrorMsg+="ANR0424W (invalid password submitted); "
+        ErrorMsg+="<a href=\"https://www.ibm.com/docs/en/spectrum-protect/8.1.16?topic=list-anr0010w#ANR0424W\" target=\"_blank\" rel=\"noopener noreferrer\">ANR0424W</a> (invalid password submitted); "
     fi
     if [ -n "$(grep ANE4042E "$ClientFile")" ]; then
-        ErrorMsg+="ANE4042E (unrecognized characters); "
+        ErrorMsg+="<a href=\"https://www.ibm.com/support/pages/ans4042e-unrecognized-characters-during-backup-data-linux-clients\" target=\"_blank\" rel=\"noopener noreferrer\">ANS4042E</a> (unrecognized characters); "
     fi
     # Deal with excessive number of filespaces
     if [ $ClientNumFilespaces -gt 10 ]; then
@@ -265,7 +247,31 @@ print_line() {
     if [ -n "$CriticalErrorMsg" ]; then
         ErrorMsg="$CriticalErrorMsg"
     fi
-    LC_NUMERIC=en_US printf "$FormatStringConten\n" "$client" "$BackedupNumfiles" "$TransferredVolume" "$BackeupElapsedtime" "${BackupStatus/ERROR/- NO BACKUP -}" "$ClientTotalNumFiles" "$ClientTotalSpaceUsedMB" "${ClientNumFilespaces:-0}" "$ClientVersion" "$ClientLastNetwork" "$ClientOS" "${ErrorMsg%; }" >> $ReportFile
+    # Set colors
+    case "$BackupStatus" in
+        "NEVER" ) TextColor=" style=\"color: red\"" ;;
+        * ) TextColor="" ;;
+    esac
+    # Deal with a backup that kind of works but hasn't been run in a while
+    if [ "${BackupStatus:0:2}" -gt 30 2>/dev/null ]; then
+        TextColor=" style=\"color: orange\""
+    fi
+    # Deleted line - saved for precautions:
+    # <td align=\"left\" $TextColor><a href=\"${OC_URL/BACKUPNODE/$client}\">$client</a></td>
+    echo "        <tr class="clients">
+          <td align=\"left\" $TextColor><a href=\"${client,,}.html\">$client</a></td>
+          <td align=\"right\"$TextColor>$(printf "%'d" $BackedupNumfiles)</td>
+          <td align=\"right\"$TextColor>$TransferredVolume</td>
+          <td align=\"left\" $TextColor>$BackeupElapsedtime</td>
+          <td align=\"left\" $TextColor>${BackupStatus/ERROR/- NO BACKUP -}</td>
+          <td align=\"left\" $TextColor>$ClientLastNetwork</td>
+          <td align=\"right\"$TextColor>$(printf "%'d" $ClientTotalNumFiles)</td>
+          <td align=\"right\"$TextColor>$(printf "%'d" $ClientTotalSpaceUsedMB)</td>
+          <td align=\"right\"$TextColor>${ClientNumFilespaces:-0}</td>
+          <td align=\"left\" $TextColor>$ClientVersion</td>
+          <td align=\"left\" $TextColor>$ClientOS</td>
+          <td align=\"left\" $TextColor>${ErrorMsg%; }</td>
+        </tr>" >> $ReportFileHTML
 }
 
 
@@ -284,14 +290,18 @@ server_info
 # Get the activity log for today (saves time to do it only one)
 # Do not include 'ANR2017I Administrator ADMIN issued command:'
 ActlogToday="$(dsmadmc -id="$ID" -password="$PASSWORD" -TABdelimited "query act begindate=today begintime=00:00:00 enddate=today endtime=now" | grep -v "ANR2017I")"
-# Get a notification if a client have more than one 'ANE4961I'; if so, there are two clients executing and that should be rectified
-DualExecutionsToday="$(echo "$ActlogToday" | grep ANE4961I | awk '{print $7}' | sed 's/)//' | sort | uniq -d)"  # Ex: DualExecutionsToday=NIKLAS
-# Get all concluded executions (ANR2579E or ANR2507I) the last year. This will save a lot of time later on
+# Get all concluded executions (ANR2579E or ANR2507I) the last $ActLogLength. This will save a lot of time later on
 AllConcludedBackups="$(dsmadmc -id="$ID" -password="$PASSWORD" -TABdelimited "query act begindate=today-$ActLogLength enddate=today" | grep -E "ANR2579E|ANR2507I")"
 
-echo "$Today $(date +%T): Backup report for ${Explanation% & } on server $ServerName (Spectrum Protect version $ServerVersion)" > $ReportFile
-printf "$FormatStringHeader\n" "CLIENT" "NumFiles" "Transferred" "Duration" "Status" " ∑ files" "Total [MB]" "# FS" "Version" "Client network" "Client OS" "Errors" >> $ReportFile
+echo "To: $RECIPIENT" > $ReportFileHTML
+echo "Subject: Backup report for ${SELECTION%; }" >> $ReportFileHTML
+echo "Content-Type: text/html" >> $ReportFileHTML
+echo  >> $ReportFileHTML
 
+REPORT_H1_HEADER="Backup report for “${SELECTION%; }”"
+REPORT_DATE="$(date +%F)"
+REPORT_HEAD="<p align=\"left\">Backup report for ${Explanation% & } on server “$ServerName” (running <a href=\"https://www.ibm.com/docs/en/spectrum-protect/8.1.16?topic=concepts-spectrum-protect-overview\">Spectrum Protect</a> version <a href=\"https://www.ibm.com/docs/en/spectrum-protect/8.1.16?topic=servers-whats-new\">$ServerVersion</a>) "
+cat "$HTML_Template_Head" | sed "s/REPORT_H1_HEADER/$REPORT_H1_HEADER/" | sed "s;REPORT_DATE;$REPORT_DATE;" | sed "s;REPORT_HEAD;$REPORT_HEAD;" >> $ReportFileHTML
 
 # Loop through the list of clients
 for client in $CLIENTS
@@ -314,17 +324,33 @@ do
 
     # Print the result
     print_line
-    #rm $ClientFile
+    rm $ClientFile
+
+    # Do the digging for each $client
+    "$ScriptDirName/inspect_one_client_html.sh" "$client" &
 done
 
 # Calculate elapsed time
 Then=$(date +%s)
 ElapsedTime=$(( Then - Now ))
+REPORT_TIME="$(date +%T)"
+REPORT_GENERATION_TIME="$((ElapsedTime%3600/60))m $((ElapsedTime%60))s"
 
-# Print a finish line
-echo "END (time: $((ElapsedTime%3600/60))m $((ElapsedTime%60))s)" >> $ReportFile
+cat "$HTML_Template_End" | sed "s/REPORT_TIME/$REPORT_TIME/" | sed "s/REPORT_GENERATION_TIME/$REPORT_GENERATION_TIME/" >> $ReportFileHTML
 
 # Send an email report (but only if there is a $RECIPIENT
 if [ -n "$RECIPIENT" ]; then
-    mailx -s "Backuprapport for ${SELECTION%; }" "$RECIPIENT" < "$ReportFile"
+    # Used to be 'mailx' but that doesn't work anymore for some reason. So, using 'sendmail'
+    #mailx -s "Backuprapport for ${SELECTION%; }" "$RECIPIENT" < "$ReportFile"
+    cat "$ReportFileHTML" | /sbin/sendmail -t
+fi
+
+# Copy result if SCP=true
+if $SCP; then
+    scp_file="$(mktemp)"
+    # Trim the output file from the initial lines (that are only for email sending)
+    sed -n '5,$p' "$ReportFileHTML" > "$scp_file"
+    chmod 644 "$scp_file"
+    scp "$scp_file" "${SCP_USER}@${SCP_HOST}:${SCP_DIR}${SELECTION/_/\/}/index.html"
+    rm "$scp_file"
 fi
